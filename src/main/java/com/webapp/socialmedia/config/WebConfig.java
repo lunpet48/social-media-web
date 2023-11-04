6a2933cf108c13ba6081fb613d2402d3266e24dc
khoihuynh300 <khoihuynh300@gmail.com>
2023-11-04 17:09:08 +0700

    update cors policy: fix error call api login with credentials:include

--- src/main/java/com/webapp/socialmedia/config/WebConfig.java
+++ src/main/java/com/webapp/socialmedia/config/WebConfig.java
@@ -9,6 +9,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*").allowedMethods("*");
+        registry
+                .addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowCredentials(true);
     }
 }
