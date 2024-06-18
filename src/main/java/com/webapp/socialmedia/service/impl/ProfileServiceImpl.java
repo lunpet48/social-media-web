8b68e235939d95a975c03a398b392e62054ad00b
lunpet48 <phaboss231@gmail.com>
2024-06-18 22:05:00 +0700

    [fix] chặn profile

--- src/main/java/com/webapp/socialmedia/service/impl/ProfileServiceImpl.java
+++ src/main/java/com/webapp/socialmedia/service/impl/ProfileServiceImpl.java
@@ -5,13 +5,16 @@ import com.webapp.socialmedia.dto.responses.ProfileResponse;
 import com.webapp.socialmedia.dto.responses.UserProfileResponse;
 import com.webapp.socialmedia.entity.Media;
 import com.webapp.socialmedia.entity.Profile;
+import com.webapp.socialmedia.entity.Relationship;
 import com.webapp.socialmedia.entity.User;
+import com.webapp.socialmedia.enums.RelationshipStatus;
 import com.webapp.socialmedia.exceptions.BadRequestException;
 import com.webapp.socialmedia.exceptions.EmptyFileException;
 import com.webapp.socialmedia.exceptions.UserNotFoundException;
 import com.webapp.socialmedia.mapper.ProfileMapper;
 import com.webapp.socialmedia.mapper.UserMapper;
 import com.webapp.socialmedia.repository.ProfileRepository;
+import com.webapp.socialmedia.repository.RelationshipRepository;
 import com.webapp.socialmedia.repository.UserRepository;
 import com.webapp.socialmedia.service.CloudService;
 import com.webapp.socialmedia.service.IProfileService;
@@ -26,6 +29,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Optional;
 import java.util.Set;
 
 
@@ -36,6 +40,7 @@ public class ProfileServiceImpl implements IProfileService {
     private final UserRepository userRepository;
     private final ProfileRepository profileRepository;
     private final CloudService cloudService;
+    private final RelationshipRepository relationshipRepository;
 
     private final Validator validator;
     private final UserMapper userMapper;
@@ -65,6 +70,11 @@ public class ProfileServiceImpl implements IProfileService {
                 .orElseGet(() -> userRepository.findByUsername(userId)
                 .orElseThrow(() -> new UsernameNotFoundException("User Not Found")));
         User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        Optional<Relationship> relationship = relationshipRepository.findByUserIdAndRelatedUserId(user.getId(), currentUser.getId());
+        if (relationship.isPresent() && (relationship.get().getStatus().equals(RelationshipStatus.BLOCK) || relationship.get().getStatus().equals(RelationshipStatus.BLOCKED))) {
+            return UserProfileResponse.builder().build();
+        }
 
         return userMapper.userToUserProfileResponse(user, currentUser.getId());
     }
